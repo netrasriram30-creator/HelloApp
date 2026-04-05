23208df3897d77bb986fcf772df0b5191f59e8df
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-06 00:11:08 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,26 @@
+/**
+ * HelloApp.java - A simple Java application that greets multiple users by name if
+ * provided as command-line arguments, or defaults to greeting "World" if no names are given.
+ *
+ * @author netrasriram30-creator
+ * @version 4.0
+ * @since UC1
+ */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }

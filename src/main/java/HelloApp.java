9853dafdb6db15735c7c589b7d0eab1aa5e02f67
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-05 23:34:27 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,16 @@
+/**
+ * HelloApp.java - A simple Java application that extends the functionality of
+ * HelloAppUC1 by accepting a user's name as a command-line argument and displaying
+ * a personalized greeting.
+ *
+ * @author netrasriram30-creator
+ * @version 1.0
+ * @since UC1
+ */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Get the name from command-line argument
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }

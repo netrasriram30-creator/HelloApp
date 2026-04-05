7ea8e868bf06c61c7b2fb1881d4da074a555c76b
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-06 00:19:51 +0530

    Coded UC6: Display Hello with multiple args using substring to remove trailing delimiter

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,27 @@
+/**
+ * HelloApp.java - A simple Java application that accepts multiple names as
+ * command-line arguments and displays a personalized greeting for each user using
+ * substring to remove the trailing delimiter.
+ * If no names are provided, it should display "Hello, World!".
+ *
+ * @author netrasriram30-creator
+ * @version 6.0
+ * @since UC1
+ */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (String arg : args) {
+                nameBuilder.append(arg).append(", ");
+            }
+            if (nameBuilder.length() > 0) {
+                name = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }

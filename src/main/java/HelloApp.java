6717177c18bcd57d39d9d0054c0e65392f1691e5
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-05 23:28:54 +0530

    Coded UC1: Display Hello World in console

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,3 +1,10 @@
+/**
+ * HelloApp.java - A simple Java application that Displays "Hello, World!" to
+ * the console.
+ *
+ * @author netrasriram30-creator
+ * @version 1.0
+ */
 public class HelloApp {
     public static void main(String[] args) {
         System.out.println("Hello, World!");

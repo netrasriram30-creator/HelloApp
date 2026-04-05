1ec9ee293829fe26257a990ec15bce49ce3a3e61
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-06 00:24:33 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,20 @@
+/**
+ * HelloApp.java - A simple Java application that greets multiple users by name if
+ * provided as command-line arguments using the String.join() method, or defaults
+ * to greeting "World" if no names are given.
+ *
+ * @author netrasriram30-creator
+ * @version 6.0
+ * @since UC1
+ */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String names = "World";
+
+        if (args.length > 0) {
+            names = String.join(", ", args);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }

12b37c304833eafd484fa622c856266d0ff407eb
netrasriram30-creator <netrasriram30@gmail.com>
2026-04-06 00:15:13 +0530

    Coded UC5: Display Hello with multiple args using enhanced for loop or default

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,29 @@
+/**
+ * HelloApp.java - A simple Java application that accepts multiple names as
+ * command-line arguments and displays a personalized greeting for each user using
+ * an enhanced for loop. If no names are provided, it should display "Hello, World!".
+ *
+ * @author netrasriram30-creator
+ * @version 5.0
+ * @since UC1
+ */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+            for (String arg : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(arg);
+                first = false;
+            }
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
